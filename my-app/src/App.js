e65bc3bbf93b0c277dd0f83790f50cb6fdc6b14b
umarul-fr <umarul.f@edstem.com>
2023-12-21 13:56:00 +0530

    2nd commit
    2nd commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 const Sort = () => {
   const [array, setArray] = useState([2, 7, 4, 3, 6, 8]);
+  const [isSorted, setIsSorted] = useState(false);
 
   const bubbleSort = () => {
     let n = array.length;
@@ -10,22 +11,26 @@ const Sort = () => {
     for (let i = 0; i < n - 1; i++) {
       for (let j = 0; j < n - 1 - i; j++) {
         setTimeout(() => {
-           if (copyarray[j] > copyarray[j + 1]) {
-             let temp = copyarray[j];
-              copyarray[j] = copyarray[j + 1];
-              copyarray[j + 1] = temp;
-              setArray([...copyarray]);
-            }
-        }, j * 1000);
+          if (copyarray[j] > copyarray[j + 1]) {
+            let temp = copyarray[j];
+            copyarray[j] = copyarray[j + 1];
+            copyarray[j + 1] = temp;
+            setArray([...copyarray]);
+          }
+        }, (i * (n - 1) + j) * 500);
       }
-      
     }
+    setIsSorted(true);
   };
 
   return (
     <div>
-      <p> Sorted Array : {array}</p>
       <button onClick={bubbleSort}>Sort Array</button>
+      {isSorted && (
+        <p style={{ color: "green", fontSize: "50px" }}>
+          Sorted Array: {array}
+        </p>
+      )}
     </div>
   );
 };
@@ -35,11 +40,10 @@ const App = () => {
 
   return (
     <div>
-      <p> original Array : {og}</p>
+      <p style={{ fontSize: "25px" }}> original Array : {og}</p>
       <Sort />
     </div>
   );
 };
 
 export default App;
-
